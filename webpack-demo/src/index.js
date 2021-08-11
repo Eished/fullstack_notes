5abc1589a66771ef36c7991d0e8ae9e6ee4642dd
Eished <kished@outlook.com>
2021-08-11 08:16:51 -0700

    webpack配置学习完结;

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,18 +1,14 @@
-import printMe from './print.js';
-import _ from 'lodash';
+import('./index.scss');
 
-function component () {
-  const element = document.createElement('div');
-  const btn = document.createElement('button');
-
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
-
-  return element;
+if (module.hot) {
+  module.hot.accept();
 }
 
-document.body.appendChild(component());
+if (process.env.NODE_ENV === 'development') {
+  console.log('localhost:');
+} else {
+  console.log('imooc.com');
+}
+
+import printMe from './print';
+printMe();
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,15 +1,48 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
+  mode: 'development',
   entry: {
     index: './src/index.js',
-    print: './src/print.js',
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    hot: true,
+  },
+  optimization: {
+    minimize: true, // 开发环境下启用 CSS 优化
+    minimizer: [
+      // 在 webpack@5 中，你可以使用 `...` 语法来扩展现有的 minimizer（即 `terser-webpack-plugin`），将下一行取消注释
+      new CssMinimizerPlugin(),
+      new TerserPlugin(),
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /.s?css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: '管理输出',
+      filename: 'index.html',
+      template: './src/template.html',
     }),
+    // 生成css文件
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    })
   ],
   output: {
     filename: '[name].bundle.js',
