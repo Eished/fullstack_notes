62a3c122775ae4e2e149c489662851b524b39266
Eished <kished@outlook.com>
2021-08-11 05:46:47 -0700

    管理输出

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,13 +1,18 @@
+import printMe from './print.js';
 import _ from 'lodash';
 
- function component() {
-   const element = document.createElement('div');
+function component () {
+  const element = document.createElement('div');
+  const btn = document.createElement('button');
 
-  // lodash（目前通过一个 script 引入）对于执行这一行是必需的
-  // lodash 在当前 script 中使用 import 引入
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-   return element;
- }
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
 
- document.body.appendChild(component());
+  element.appendChild(btn);
+
+  return element;
+}
+
+document.body.appendChild(component());
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,9 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: '管理输出',
+    }),
+  ],
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
 };
