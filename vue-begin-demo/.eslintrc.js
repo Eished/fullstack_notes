12c658fd99f1c08956a3b2c08aef2ad04b626a65
Eished <kished@outlook.com>
2021-08-12 08:11:21 -0700

    增加vue配置文件;

---
+++ vue-begin-demo/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  parserOptions: {
+    parser: "babel-eslint",
+  },
+  "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+};
---
+++ vue-begin-demo/vue.config.js
@@ -0,0 +1,6 @@
+// vue.config.js
+module.exports = {
+  configureWebpack: {
+    devtool: "source-map",
+  },
+};
