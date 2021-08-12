13b39e582a00355220e2e1fd079e2da6457f53cf
Eished <kished@outlook.com>
2021-08-12 02:43:02 -0700

    代码风格格式化

--- gulp-demo/gulpfile.js
+++ gulp-demo/gulpfile.js
@@ -1,44 +1,44 @@
-const { src, dest, series, watch } = require('gulp')
+const { src, dest, series, watch } = require('gulp');
 const browserSync = require('browser-sync').create();
 
 // 压缩js
-function js (cb) {
-  console.log("Hello scripts!");
+function js(cb) {
+  console.log('Hello scripts!');
   // [gulp-uglify]
   src('js/*.js')
     .pipe(dest('./dist/js'))
-    .pipe(browserSync.reload({ stream: true }))
+    .pipe(browserSync.reload({ stream: true }));
   cb();
 }
 
 // 压缩scss
-function css (cb) {
+function css(cb) {
   // gulp-sass
-  console.log("Hello styles!");
+  console.log('Hello styles!');
   src('css/*.css')
     .pipe(dest('./dist/css'))
-    .pipe(browserSync.reload({ stream: true }))
+    .pipe(browserSync.reload({ stream: true }));
   cb();
 }
 
 // 监听文件变化
-function watcher (cb) {
+function watcher(cb) {
   watch('js/*.js', js);
-  watch('css/*css', css);
+  watch('css/*.css', css);
   cb();
 }
 
 // 删除dist目录内容
-function clean (cb) {
+function clean(cb) {
   // gulp-dest-clean
-  console.log("clean!");
+  console.log('clean!');
   cb();
 }
 
 // server 任务 // Static Server + watching scss/html files
-function serve (cb) {
+function serve(cb) {
   browserSync.init({
-    server: "./"
+    server: './',
   });
   cb();
 }
@@ -46,10 +46,4 @@ function serve (cb) {
 exports.scripts = js;
 exports.styles = css;
 exports.clean = clean;
-exports.default = series([
-  clean,
-  js,
-  css,
-  serve,
-  watcher
-])
+exports.default = series([clean, js, css, serve, watcher]);
