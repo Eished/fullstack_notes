40c215f64f5b25c60034ca951593b64500c53e87
Eished <kished@outlook.com>
2022-02-08 06:15:55 +0000

    add express mock api

---
+++ Aws-Lambda-TypeScript/express-mock-api/src/model/resModel.js
@@ -0,0 +1,42 @@
+class BaseModel {
+  constructor(data, message) {
+    if (data) {
+      this.data = data;
+    }
+    if (message) {
+      this.message = message;
+    }
+  }
+}
+
+class SuccessModel extends BaseModel {
+  constructor(key, statusCode = 200, data, message) {
+    super(message);
+
+    if (key && statusCode) {
+      this[key] = {
+        StatusCode: statusCode,
+      };
+    }
+    if (data) {
+      this[key] = { ...this[key], ...data };
+    }
+  }
+}
+
+class ErrorModel extends BaseModel {
+  constructor(statusCode, message) {
+    super();
+    this.details = [
+      {
+        code: statusCode,
+        message,
+      },
+    ];
+  }
+}
+
+module.exports = {
+  SuccessModel,
+  ErrorModel,
+};
