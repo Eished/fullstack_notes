07dae5fa4b97946a2ae8bf296a6a6efd674f8c78
eished <kished@outlook.com>
2022-06-15 02:45:22 +0800

    add svg

--- vue3-webpack/src/App.vue
+++ vue3-webpack/src/App.vue
@@ -1,16 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <svg-icon icon-class="haizhe"></svg-icon>
+    <!-- <svg-icon icon-class="doufu"></svg-icon> -->
+    <HelloWorld />
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import svgIcon from './components/svgIcon/'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    svgIcon
+  },
 }
 </script>
 
--- vue3-webpack/src/main.js
+++ vue3-webpack/src/main.js
@@ -1,4 +1,6 @@
+import '@/assets/icons'
+import svgIcon from '@/components/svgIcon/index.vue'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).component('svg-icon', svgIcon).mount('#app')
